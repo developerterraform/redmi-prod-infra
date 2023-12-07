a25f58e594c3b30eb8f1325a82998b545a7995d9
ranji <ranjith@gmail.com>
2023-12-07 18:38:30 +0000

    Monitoring 8080

--- main.tf
+++ main.tf
@@ -36,6 +36,15 @@ resource "aws_security_group" "http_access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "HTTP_INCOMING"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
