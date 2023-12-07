a842ac1a644208b92da668dac175245ca2f5080b
ranji <ranjith@gmail.com>
2023-12-07 18:22:52 +0000

    Remote port 22 is enabled

--- main.tf
+++ main.tf
@@ -51,4 +51,32 @@ resource "aws_security_group" "http_access" {
     Owner   = var.project_owner
   }
 }
+resource "aws_security_group" "remote_access" {
+  name        = "${var.project_name}-${var.project_env}-remote_access"
+  description = "${var.project_name}-${var.project_env}-remote_access"
 
+
+  ingress {
+    description      = "Remote-Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-remote_access"
+    Project = var.project_name
+    Env     = var.project_env
+    Owner   = var.project_owner
+  }
+}
